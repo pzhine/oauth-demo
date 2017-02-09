e79d62ced6160b16b412616d79e4c3ff67303bf2
Paul Hine <paul.hine@gmail.com>
2017-02-09 16:48:38 -0500

    use port from env

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('./public'));
 
 app.get('/oauth', function(req, res) {
@@ -9,6 +11,6 @@ app.get('/oauth', function(req, res) {
   res.status(200).end();
 });
 
-app.listen(80, function() {
-  console.log('server started on port 80')
+app.listen(app.get('port'), function() {
+  console.log('server started on port', app.get('port'))
 });
