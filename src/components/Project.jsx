114bb8f88dcd1605828287a91cbca386ff351f39
Paul Hine <paul.hine@gmail.com>
2017-02-09 19:56:18 -0500

    Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import fetch from 'isomorphic-fetch';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+import base64 from 'base-64';
 
 const Project = React.createClass({
+  getInitialState() {
+    return {
+      projectJson: null
+    }
+  },
   componentWillMount() {
-    const url = 'https://raw.githubusercontent.com/pzhine/oauth-demo/master/upnext.json';
-    const options = {
-      mode: 'no-cors'
-    };
-    fetch(url, options).then((data) => {
-      return data.blob();
-    }).then((data) => {
-      console.log(data);
+    const url = 'https://api.github.com/repos/pzhine/oauth-demo/contents/upnext.json';
+    fetch(url)
+    .then((response) => {
+      console.log(response.status);
+      return response.json();
+    })
+    .then((json) => {
+      const projectJson = JSON.parse(base64.decode(json.content));
+        console.log(projectJson);
+      this.setState({
+        projectJson: projectJson
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 
   },
@@ -18,6 +32,12 @@ const Project = React.createClass({
     return (
       <div>
         Hi
+        <div>
+          {this.state.projectJson 
+            ? this.state.projectJson.project.name 
+            : 'loading...'
+          }
+        </div>
       </div>
     );
   }
